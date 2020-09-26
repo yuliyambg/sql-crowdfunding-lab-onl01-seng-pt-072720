bea63fbb4c1c7f1b0bd2263cf9a63ebad9d69be3
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-26 23:24:25 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name,age) VALUES 
+("Ann",25), ("Joe",26),("Toe",27),("Mia",28),("Lea",29),
+("Ronny",35),("Bonnie",45),("Eddie",25),("Ed",35),("Ivan",55),
+("Vania",45),("Taylor",35),("Michelle",75),("Doren",55),
+("Dorin",35),("Ann",26),("Belle",28),("Bella",25),("Lorie",25),("Lea",25);
