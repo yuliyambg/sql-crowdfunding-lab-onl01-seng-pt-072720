ca92bb5d5ef938545b7d35f889cf873caec9f776
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-26 23:35:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,3 +4,6 @@ INSERT INTO users (name,age) VALUES
 ("Vania",45),("Taylor",35),("Michelle",75),("Doren",55),
 ("Dorin",35),("Ann",26),("Belle",28),("Bella",25),("Lorie",25),("Lea",25);
 
+INSERT INTO projects (title,category, funding_goal, start_date, end_date) VALUES 
+("Good", "art", 50000, '09-29-2020', '09-30-2020'), ("More", "art", 30000, '09-29-2020', '10-30-2020'),("Better", "life", 50000, '09-29-2020', '09-30-2020'),("Grand", "finance", 45000, '09-29-2020', '10-28-2020'),("Good", "art", 45000, '09-29-2020', '09-30-2020'),
+("Best", "art", 60000, '09-29-2020', '09-30-2020'),("Lake", "grocery", 50000, '09-29-2020', '09-30-2020'),("Ground", "construction", 50000, '09-29-2020', '09-30-2020'),("Glass", "art", 50000, '09-29-2020', '09-30-2020'),("God", "prayers", 50000, '09-29-2020', '09-30-2020');
