463e4529d6a232b4d33d3d1fce94b719233bfaca
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 00:37:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,10 +16,11 @@ ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, pledges.amount
+  "SELECT users.name, users.age, SUM(pledges.amount)
 FROM users
 JOIN pledges
 ON users.id = pledges.user_id
+GROUP BY users.name
 ORDER BY users.name;"
 end
 
